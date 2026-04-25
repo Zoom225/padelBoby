1ed5c1ff0cb5a433a0ead5af3fd2595ffad2522b
mise-a-jour-backend <bobypro225@gmail.com>
2026-04-25 21:53:16 +0200

    initialisation de ripo

---
+++ backend/src/main/java/padelFilm/padelFilm/PadelFilmApplication.java
@@ -0,0 +1,13 @@
+package padelFilm.padelFilm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PadelFilmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PadelFilmApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/padelFilm/padelFilm/PadelFilmApplicationTests.java
@@ -0,0 +1,13 @@
+package padelFilm.padelFilm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PadelFilmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
